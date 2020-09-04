45fdaf88dacfb7e253cd7f943836d79968b3104e
Kushal Neupane <mkushal10@gmail.com>
2020-09-04 12:34:28 +0545

    2 commit

---
+++ 2/2. program.py
@@ -0,0 +1,8 @@
+length=float(input("Enter the length of rectangle: "))
+breadth=float(input("Enter the breadth of rectangle: "))
+
+area=length*breadth
+perimeter=2*(length+breadth)
+
+print("The area of rectangle is ", area)
+print("The perimeter of rectangle is ", perimeter)
